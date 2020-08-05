eabdd13a4f6cf61a2ce5677d61f9b146075bad7d
Kaleo-Stark <kaleovieira.dev@gmail.com>
2020-08-04 23:15:43 -0300

    funçao que navega ate o facebook e faz login

--- index.js
+++ index.js
@@ -9,3 +9,22 @@ const linkPagina = ''; // ................. LINK DA PAGINA DO FACEBOOK, LINK JÁ
 
 const linkPublicacao = ''; // ............. LINK DA PAGINA WEB COM O CONTEÚDO QUE DESEJA PUBLICAR O PREVIEW
 // Exemplo: 'https://linkPublicacao.com.br/publicao.html'.
+
+async function logarFacebook(login, senha, pagina) { // FUNÇÃO DE LOGIN NO FACEBOOK
+    console.log('Navegando para o facebook ...');
+    await pagina.goto('https://web.facebook.com/');  // ................... VAI PARA A PÁGINA DO FACEBOOK.
+
+    console.log('Digitando login ...');
+    await pagina.focus('#email'); // ...................................... FOCA O CURSOR NO INPUT DE LOGIN.
+    await pagina.keyboard.type(login); // ................................. DIGITA O LOGIN.
+
+    console.log('Digitando senha ...');
+    await pagina.focus('#pass'); // ....................................... FOCA O CURSOR NO INPUT DE SENHA.
+    await pagina.keyboard.type(senha); // ................................. DIGITA A SENHA.
+
+    const form = await pagina.$('#login_form');  // ....................... CAPTURA O FORMULARIO DE ACESSO.
+
+    await form.evaluate(form => form.submit()); // ........................ ENVIA LOGIN E SENHA PARA VALIDAÇÃO.
+    console.log('logando')
+    await pagina.waitForNavigation({ waitUntil: 'networkidle0' }); // ..... ESPERA A PAGINA NAVEGAR PARA A CONTA.
+}
