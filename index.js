ae7217a551959a4600afa7e2a69f10352d3cea04
Kaleo-Stark <kaleovieira.dev@gmail.com>
2020-08-04 23:11:01 -0300

    importaçao da biblioteca puppeteer e definiçao de variaveis

---
+++ index.js
@@ -0,0 +1,11 @@
+const puppeteer = require('puppeteer'); // IMPORTAÇÃO DA BIBLIOTECA PUPPETER
+
+const login = ''; // ...................... LOGIN PARA LOGAR NO FACEBOOK.
+
+const senha = ''; // ...................... SENHA PARA LOGAR NO FACEBOOK.
+
+const linkPagina = ''; // ................. LINK DA PAGINA DO FACEBOOK, LINK JÁ NA AREA DE POSTAGEM.
+// Exemplo: 'https://web.facebook.com/pg/<REFERENCIA DA PAGINA>/posts/?ref=page_internal'.
+
+const linkPublicacao = ''; // ............. LINK DA PAGINA WEB COM O CONTEÚDO QUE DESEJA PUBLICAR O PREVIEW
+// Exemplo: 'https://linkPublicacao.com.br/publicao.html'.
